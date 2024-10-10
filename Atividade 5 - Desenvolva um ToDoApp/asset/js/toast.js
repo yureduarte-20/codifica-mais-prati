1144a71bfde20280fedb4a86ba0c67a44d5089a8
Yure <yuresamarone34@gmail.com>
2024-10-10 12:38:18 -0300

    feature: atividade 5

---
+++ Atividade 5 - Desenvolva um ToDoApp/asset/js/toast.js
@@ -0,0 +1,13 @@
+
+window.toast = {
+    showToast(msg, id = '#success-toast') {
+        const toastElement = document.querySelector(id);
+        const toast = window.bootstrap.Toast.getOrCreateInstance(toastElement);
+        toastElement.querySelector('.toast-body').textContent = msg;
+        toast.show()
+        const t = setTimeout(() => {
+            toast.hide()
+            clearTimeout(t)
+        }, 5000)
+    },
+}
